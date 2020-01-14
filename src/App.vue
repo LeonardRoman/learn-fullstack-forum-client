c1912dcf3a6934864684a88a13dc65ebd8398772
LeonardRoman <leonardroman1@gmail.com>
2020-01-14 19:02:46 +0400

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <app-nav />
+    <router-view class="container" />
+  </div>
+</template>
+
+<script>
+  /* eslint-disable import/extensions */
+
+  import AppNav from './components/AppNav'
+
+  export default {
+    components: {
+      AppNav
+    }
+  }
+</script>
+
+<style lang="scss">
+</style>
